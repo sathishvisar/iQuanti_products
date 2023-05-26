ca17817fd304b09e5ef016d2f39ceb64e16174ef
Sathishkumar <sathish.visar@gmail.com>
2023-05-27 03:26:50 +0530

    code clearance

--- src/services/products.js
+++ src/services/products.js
@@ -2,21 +2,10 @@ import axios from 'axios'
 
 export const fetchProducts = async (url = 'https://dev1api.credello.com/static-products') => {
   try {
-    await delay(1000)
     const response = await axios.get(url)
 
     return [response?.data, null]
   } catch ({ response }) {
     return [null, response && response.data]
   }
-}
-
-const delay = async (milliseconds) => {
-  await new Promise((resolve) => {
-    setTimeout(() => {
-      resolve()
-    }, milliseconds)
-  })
-
-  console.log(`This is executed after ${milliseconds} milliseconds.`)
 }
